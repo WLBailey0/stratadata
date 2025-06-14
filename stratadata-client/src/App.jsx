7c1feaac4df05fa7a5808957a7043041235831fd
William Bailey <WLBailey0@gmail.com>
2025-06-14 15:25:48 -0500

    add login to ui

--- stratadata-client/src/App.jsx
+++ stratadata-client/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/App.css'
 import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage';
+import { UserContext, UserProvider } from './contexts/userContext';
 
 
 function App() {
   return (
     <div>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/" element={<HomePage />} /> */}
-      </Routes>
-    </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </div>
   )
 }
